facb36c182aea4e17d9d862e477997e17982e7e4
Volkan Tufekci <volkan.tufekci@crenno.com>
2013-01-03 00:00:24 +0200

    Quit adding date to the name of log directory. Now it is always under vlog dir

--- src/main/java/com/volkan/Utility.java
+++ src/main/java/com/volkan/Utility.java
@@ -1,12 +1,11 @@
 package com.volkan;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Utility {
 
 	public static String buildLogFileName () {
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddhhmm");
-		return "hede" + simpleDateFormat.format(new Date());
+//		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddhhmm");
+//		return "hede" + simpleDateFormat.format(new Date());
+		return "vlog";
 	}
 }
