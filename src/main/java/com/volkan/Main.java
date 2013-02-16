fa38de6d478f1313000eb9b64201669f2e29d2fe
Volkan Tufekci <volkan.tufekci@crenno.com>
2013-02-16 14:28:51 +0200

    sample interpartition.properties added.

--- src/main/java/com/volkan/Main.java
+++ src/main/java/com/volkan/Main.java
@@ -40,7 +40,7 @@ public class Main {
 //		ExecutionEngine engine = new ExecutionEngine(db);
 		
 		long start = System.currentTimeMillis();
-		int hede = 0;
+		int hede = 14;
 		try {
 			switch (hede) {
 			case 0:
