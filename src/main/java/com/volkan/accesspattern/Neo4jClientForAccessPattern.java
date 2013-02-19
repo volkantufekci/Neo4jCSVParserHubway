db1e775f6bc55d3357089edd746c11c458d8f1b7
Volkan Tufekci <volkan.tufekci@crenno.com>
2013-02-20 01:40:23 +0200

    writeNodeIDGidMapForRuby is implemented

--- src/main/java/com/volkan/accesspattern/Neo4jClientForAccessPattern.java
+++ src/main/java/com/volkan/accesspattern/Neo4jClientForAccessPattern.java
@@ -1,5 +1,8 @@
 package com.volkan.accesspattern;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +21,7 @@ public class Neo4jClientForAccessPattern {
 	}
 	
 	protected Map<Long, List<Long>> collectNodeIDNeiIDsMap( GraphDatabaseService dbAP, 
-															int maxNodeCount ) 
+															int maxNodeCount ) throws IOException 
 	{
 		Map<Long, List<Long>> nodeIDNeiIDArrayMap = new HashMap<Long, List<Long>>();
 		
@@ -34,6 +37,8 @@ public class Neo4jClientForAccessPattern {
 			nodeIDNeiIDArrayMap.put(node.getId(), neiIDs);
 		}
 		
+		writeNodeIDGidMapForRuby();
+		
 		return nodeIDNeiIDArrayMap;
 	}
 
@@ -44,6 +49,21 @@ public class Neo4jClientForAccessPattern {
 		}
 	}
 
+	private void writeNodeIDGidMapForRuby() throws IOException {
+		BufferedWriter gpartInputFile = new BufferedWriter(new FileWriter("gpartInputFile"));
+		
+		StringBuilder sb = new StringBuilder();
+		for (Long nodeID : nodeIDGidMap.keySet()) {
+			long gid = nodeIDGidMap.get(nodeID);
+			sb.append(nodeID + "," + gid + "\n");
+		}
+		
+		gpartInputFile.write( sb.toString() );
+		
+		if (gpartInputFile != null)
+			gpartInputFile.close();
+	}
+	
 	public Map<Long, Long> getNodeIDGidMap() {
 		return nodeIDGidMap;
 	}
