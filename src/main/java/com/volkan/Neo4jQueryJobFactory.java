148377cc9336b0d80490614898294ed9fbda95ef
Volkan Tufekci <volkan.tufekci@crenno.com>
2013-02-24 17:41:17 +0200

    JobID creation from H2 is moved into Neo4jQueryJobFactory in order to automate all the query job process for ScheduledExecutor

--- src/main/java/com/volkan/Neo4jQueryJobFactory.java
+++ src/main/java/com/volkan/Neo4jQueryJobFactory.java
@@ -24,4 +24,35 @@ public class Neo4jQueryJobFactory {
 		
 		return r;
 	}
+
+	public static Runnable buildJobWithGenarateJob(
+			final H2Client h2Client, final Map<String, Object> jsonMap) {
+		
+		Runnable r = new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					String port = jsonMap.get(JsonKeyConstants.START_PORT).toString();
+					generateJobInDBFromJsonFileName(h2Client, jsonMap);
+					Neo4jClientAsync neo4jClientAsync = new Neo4jClientAsync();
+					neo4jClientAsync.delegateQueryAsync(port, jsonMap);
+					neo4jClientAsync.periodicFetcher((long) jsonMap.get(JsonKeyConstants.PARENT_JOB_ID));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		};
+		
+		return r;
+	}
+	
+	private static Map<String, Object> generateJobInDBFromJsonFileName(
+			H2Client h2Client, Map<String, Object> jsonMap) throws Exception {
+		long jobID = h2Client.generateJob(jsonMap);
+		jsonMap.put(JsonKeyConstants.JOB_ID, jobID);
+		jsonMap.put(JsonKeyConstants.PARENT_JOB_ID, jobID);
+		return jsonMap;
+	}
 }
